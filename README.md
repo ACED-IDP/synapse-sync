4a354227029926a8447ee48b1351a5ca1eaf17d6
Brian <brian@bwalsh.com>
2024-04-12 08:17:44 -0700

    feature/improvements (#1)

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
 # synapse-sync
+
+## Overview
+
+This command-line utility facilitates the synchronization of user accounts from Sage Synapse system to University of Chicago's Gen3 system. It's designed to streamline user management processes in DevOps environments where data exchange between these systems is required.
+
+## Features
+
+* User Synchronization: Transfer user accounts from Sage's Synapse system to University of Chicago's Gen3 system seamlessly.
+* Selective Sync: Option to specify particular users for synchronization, offering flexibility and control over the process.
+* Configuration Flexibility: Easily configurable parameters for specifying source and target systems, ensuring adaptability to different environments.
+* Logging: Comprehensive logging of sync operations for auditing and troubleshooting purposes.
+
+## Installation
+
+```shell
+pip install g3t-synapse-sync
+```
+
+## Configuration
+
+* Follow [these instructions](https://aced-idp.github.io/getting-started/) to configure the g3t client.
+  * We recommend using the g3t profile 'bridg2ai' ie `export G3T_PROFILE=bridge2ai`
+
+* Follow these instructions to setup the synapse client:
+  *  https://python-docs.synapse.org/tutorials/authentication/#prerequisites
+  *  https://help.synapse.org/docs/Client-Configuration.1985446156.html#ClientConfiguration-ForDevelopers
+
+## Test
+```shell
+g3t ping
+```
+
+## Usage
+
+```shell
+$ g3t_synapse_sync teams ls --help
+Using default config
+Usage: g3t_synapse_sync teams ls [OPTIONS] TEAM_ID
+
+  List commands to add users (and current status) of a team to gen3.
+
+      TEAM_ID one of:
+      * AI_READI: https://www.synapse.org/#!Team:3499899
+      * CHoRUS: https://www.synapse.org/#!Team:3499900
+      * CM4AI: https://www.synapse.org/#!Team:3499897
+      * Voice: https://www.synapse.org/#!Team:3499898
+          
+
+# eg
+
+$ g3t_synapse_sync teams ls AI_READI -l
+Using default config
+Logging in to synapse
+Getting current gen3 users
+Syncing team: Bridge2AI AI-READI Open House Participants
+<cmd> # name status updated_time policy_id
+g3t utilities users add --username '1234567 (Synapse ID)' # user1 STATUS SIGNED 2024-04-06T23:58:31.828793 programs.bridge2ai.projects.AI_READI_reader 
+g3t utilities users add --username '7654321 (Synapse ID)' # user2 STATUS NONE
+```
--- synapse_sync/cli.py
+++ synapse_sync/cli.py
@@ -1,17 +1,21 @@
+import contextlib
 import json
+import os
 import pathlib
 import subprocess
 import sys
+from datetime import datetime
 
 import synapseclient
 import click
 import yaml
 from synapseclient import Synapse
+import gen3_util.config
 
 
 def login(debug: bool) -> Synapse:
     """Login to synapse."""
-    click.secho(f"Logging in to synapse", fg="yellow", file=sys.stderr)
+    # click.secho(f"Logging in to synapse", fg="yellow", file=sys.stderr)
 
     try:
 
@@ -67,27 +71,47 @@ def cli(ctx, config):
     if config:
         with open(config) as f:
             ctx.obj['config'] = yaml.safe_load(f)
-            click.secho(f"Using config file {config}", fg="yellow", file=sys.stderr)
+            # click.secho(f"Using config file {config}", fg="yellow", file=sys.stderr)
     else:
         ctx.obj['config'] = default_config()
-        click.secho(f"Using default config", fg="yellow", file=sys.stderr)
+        # click.secho(f"Using default config", fg="yellow", file=sys.stderr)
 
 
-def get_current_requests():
-    """Get Gen3 current requests."""
-    # run cmd get stdout
-    click.secho(f"Getting current gen3 users", fg="yellow", file=sys.stderr)
-    cmd = "g3t --format json utilities access ls --all"
+def run_cmd(cmd: str, dry_run: bool):
+    """Run a command."""
     try:
-        result = subprocess.run(cmd, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        if dry_run:
+            click.secho(f"DRY RUN: {cmd}", fg="yellow", file=sys.stderr)
+            return None
+        result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        if result.returncode != 0:
+            click.secho(f"Command '{cmd}' failed with error: {result.stderr.decode('utf-8')}", fg="red", file=sys.stderr)
+            return None
         output = result.stdout.decode('utf-8')
-        json_output = json.loads(output)
-        return json_output
+        return output
     except subprocess.CalledProcessError as e:
-        click.secho(f"Command '{cmd}' failed with error: {e}")
+        click.secho(f"Command '{cmd}' failed with error: {e}, {result.stdout.decode('utf-8')}", fg="red", file=sys.stderr)
         return None
+
+
+def get_current_requests(project):
+    """Get Gen3 current requests."""
+    # run cmd, return stdout
+
+    # click.secho(f"Getting current gen3 users", fg="yellow", file=sys.stderr)
+    try:
+        cmd = "g3t --format json utilities access ls --all"
+        output = run_cmd(cmd, dry_run=False)
+        json_output = json.loads(output)
+        assert 'requests' in json_output, f"Expected 'requests' in {json_output}"
+        return {
+            'requests': [_ for _ in json_output['requests'] if
+                         _['status'] == 'SIGNED' and project in _['policy_id'] and not _['revoke']]
+        }
+
+        return json_output
     except json.JSONDecodeError as e:
-        print(f"Failed to parse JSON output from command '{cmd}': {e}")
+        click.secho(f"Failed to parse JSON output from command '{cmd}': {e}", fg="red", file=sys.stderr)
         return None
 
 
@@ -98,12 +122,11 @@ def teams(ctx):
     pass
 
 
-@teams.command("ls")
-@click.option('--debug', is_flag=True, default=False, help="log synapse calls")
-@click.option('--long', '-l', is_flag=True, default=False, help="show user name")
-@click.argument('team_id')
+@teams.command("sync")
+@click.option('--debug', is_flag=True, default=False, help="Show debug output.")
+@click.option('--dry_run', is_flag=True, default=False, help="List cmds, don't run them")
 @click.pass_context
-def teams_ls(ctx, debug: bool, team_id: str, long: bool):
+def teams_sync(ctx, debug: bool, dry_run: bool):
     """List commands to add users (and current status) of a team to gen3.
 
     \b
@@ -114,7 +137,23 @@ TEAM_ID one of:
 * Voice: https://www.synapse.org/#!Team:3499898
     """
 
+    admin_users = """
+beckmanl@ohsu.edu
+ellrott@ohsu.edu
+leejor@ohsu.edu
+peterkor@ohsu.edu
+walsbr@ohsu.edu
+wongq@ohsu.edu
+""".strip().split('\n')
+
     try:
+        config = gen3_util.config.default()
+        assert config.gen3.project_id, "Not in a gen3 project directory, expected .g3t"
+        click.secho(f"gen3 project_id: {config.gen3.project_id}", fg="yellow", file=sys.stderr)
+
+        program, project = config.gen3.project_id.split('-')
+        # click.secho(f"using gen3 project: {project} as team_id", fg="yellow", file=sys.stderr)
+        team_id = project
 
         valid_team_ids = [_['id'] == team_id for _ in ctx.obj['config']['synapse_teams']]
         valid_teams = {_['name']: _['id'] for _ in ctx.obj['config']['synapse_teams']}
@@ -128,35 +167,100 @@ TEAM_ID one of:
                 return
             team_id = valid_teams[team_id]
 
-        project_id = 'bridge2ai-AI_READI'  # TODO - hardcoded
-        program, project = project_id.split('-')
         syn = login(debug)
 
-        current_requests = get_current_requests()
+        current_requests = get_current_requests(project)
         if not current_requests:
-            return
+            current_requests = {'requests': []}
+            click.secho(f"Failed to get current requests, proceeding, status may not be accurate.", fg="yellow", file=sys.stderr)
+        if 'requests' not in current_requests:
+            current_requests['requests'] = []
+            click.secho(current_requests['msg'], fg="yellow", file=sys.stderr)
+        assert 'requests' in current_requests, f"Expected 'requests' in {current_requests}"
+        assert len(current_requests['requests']) > 0, f"Expected 'requests' in {current_requests}"
 
         current_users = {_.get('username'): _ for _ in current_requests['requests'] if project in _['policy_id']}
-        # for k, v in current_users.items():
-        #     print(k, v['policy_id'], v['username'], v['status'], v['updated_time'])
 
         team = syn.getTeam(team_id)
         click.secho(f"Syncing team: {team.name}", fg="yellow", file=sys.stderr)
+        cmds = []
+        new_users = []
         for _ in syn.getTeamMembers(team):
-            user_name_msg = ''
-            username = f'{_.member.ownerId} (Synapse ID)'
-            if long:
-                user_name_msg = f' # {_.member.userName}'
-                if username in current_users:
-                    usr = current_users[username]
-                    user_name_msg += f" {usr['status']} {usr['updated_time']} {usr['policy_id']} "
-                else:
-                    user_name_msg += f" NOT ADDED"
-            print(f"g3t utilities users add --username '{_.member.ownerId} (Synapse ID)'{user_name_msg}")
+            username = f'{_.member.ownerId}@synapse.org'
+            cmd = f"g3t utilities users add --username"
+            user_name_msg = f' # {_.member.userName}'
+            if username in current_users:
+                usr = current_users[username]
+                user_name_msg += f" STATUS {usr['status']} {usr['updated_time']} {usr['policy_id']} "
+                click.secho(f"# '{username}'{user_name_msg}", fg="green", file=sys.stderr)
+                continue
+            else:
+                user_name_msg += f" STATUS NONE"
+                cmds.append(f"{cmd} '{username}'{user_name_msg}")
+                new_users.append(username)
 
-            # invites = syn.get_team_open_invitations(team)
-            # for invite in invites:
-            #     print(invite)
+        if cmds:
+            click.secho(f"Adding {len(cmds)} users to gen3", fg="yellow", file=sys.stderr)
+            for cmd in cmds:
+                click.secho(cmd, fg="yellow", file=sys.stderr)
+                run_cmd(cmd, dry_run=dry_run)
+            run_cmd("g3t utilities access sign", dry_run=dry_run)
+        else:
+            click.secho(f"No new users to add to {project}", fg="yellow", file=sys.stderr)
+
+        # remove users not in synapse
+        cmds = []
+        expected_users = [f'{_.member.ownerId}@synapse.org' for _ in syn.getTeamMembers(team)]
+        for user_name, user in current_users.items():
+            if user_name in admin_users:
+                click.secho(f"Skipping admin user {user_name}", fg="green", file=sys.stderr)
+                continue
+            if user_name not in expected_users:
+                cmd = f"g3t utilities users rm --username"
+                cmds.append(f"{cmd} '{user_name}' --project_id {program}-{project} # {user['status']} {user['updated_time']} {user['policy_id']}")
+                print(user_name, user)
+        if cmds:
+            click.secho(f"Removing {len(cmds)} users from gen3", fg="yellow", file=sys.stderr)
+            for cmd in cmds:
+                click.secho(cmd, fg="yellow", file=sys.stderr)
+                run_cmd(cmd, dry_run=dry_run)
+            run_cmd("g3t utilities access sign", dry_run=dry_run)
+        else:
+            click.secho(f"No users to remove from {project}", fg="yellow", file=sys.stderr)
 
     except Exception as e:
         click.secho(f"{e.__class__.__name__} {e}", fg="red", file=sys.stderr)
+        if debug:
+            raise e
+
+
+@teams.command("sync-all")
+@click.option('--program', default="bridge2ai", help="gen3 program")
+@click.option('--projects_dir', default="projects", help="root directory holding <program>-<project>")
+@click.option('--debug', is_flag=True, default=False, help="Show debug output.")
+@click.pass_context
+def teams_sync_all(ctx, program, projects_dir, debug: bool):
+    """Sync teams with gen3."""
+    try:
+        path = pathlib.Path(projects_dir)
+        start_dir = os.getcwd()
+        start_dir = pathlib.Path(start_dir)
+        team_names = [_['name'] for _ in ctx.obj['config']['synapse_teams']]
+
+        for team_name in team_names:
+            project_dir = path / f"{program}-{team_name}"
+            assert project_dir.exists(), f"Missing expected directory: {path / f'{program}-{team_name}'}"
+            project_id = project_dir.name
+            timestamp_str = datetime.now().strftime("%Y%m%d-%H%M%S")
+            log_file = pathlib.Path('logs') / f'synapse-sync-{team_name}-{timestamp_str}.log'
+            click.secho(f"Syncing project {project_id}, log {log_file}", fg="yellow", file=sys.stderr)
+            with open(log_file, 'w') as f:
+                with contextlib.redirect_stdout(f):
+                    with contextlib.redirect_stderr(f):
+                        os.chdir(project_dir.absolute())
+                        ctx.invoke(teams_sync)
+                        os.chdir(start_dir.absolute())
+    except Exception as e:
+        click.secho(f"{e.__class__.__name__} {e}", fg="red", file=sys.stderr)
+        if debug:
+            raise e
